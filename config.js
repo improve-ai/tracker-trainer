02d15321df23fb56974a6b40694b0ea057199ea4
EC2 Default User <ec2-user@ip-172-31-20-192.us-east-2.compute.internal>
2018-12-29 00:07:40 +0000

    change to  us-west-2

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     hyperparameters: {
-        defaults: {
+        "default": {
             max_age: "7776000", // 90 days
             objective: "binary:logistic",
         },
         lRgX7U2VPZ6I1DUaSUr6D8jH4iFju3MY7i3p9mbq: {
             "messages-1.0": {
-                max_age: "2592000" // 30 days
+                max_age: "1000000" //"1728000" // 20 days
             } 
         },
         lF8yFNYXiT5fIlBHQMgbY3EtPUfbjJmS1OskfqiT: {
