3071f53fb3ee14dc066e86c6a60e1e34b34b66f8
EC2 Default User <local@local>
2021-02-20 22:10:51 +0000

    simplify track

--- src/track/http_api.js
+++ src/track/http_api.js
@@ -2,16 +2,11 @@
 
 const AWS = require('aws-sdk');
 const firehose = new AWS.Firehose();
-const uuidv4 = require('uuid/v4');
-
-const customize = require("./customize.js")
-const naming = require("./naming.js")
 
 const LOG_PROBABILITY = .1;
 
 module.exports.track = async function(event, context) {
   let logging = checkShouldLog()
-  consoleTime('track', logging)
   let receivedAt = new Date()
   
   if (logging) {
@@ -20,54 +15,29 @@ module.exports.track = async function(event, context) {
 
   let body = JSON.parse(event.body)
 
-  if (!body || !body.history_id) {
-    return errorResponse("the 'history_id' field is required")
+  if (!body || !body.history_id || !body.message_id) {
+    return errorResponse("history_id and message_id fields are required")
   }
   
-  let projectName = customize.projectNameForTrack(event, context)
-
-  if (!projectName || !naming.isValidProjectName(projectName)) {
-    console.log(`WARN: invalid project name ${projectName}, not alphanumeric, underscore, dash, space, period ${JSON.stringify(event)}`)
-    return errorResponse("project misconfigured or missing credentials")
+  body["received_at"] = receivedAt.toISOString()
+  // FIX timestamp must never be in the future
+  if (!body.timestamp) {
+    body["timestamp"] = body["received_at"]
   }
-
-  return sendToFirehose(projectName, body, receivedAt, logging).then(() => {
-    consoleTimeEnd('track', logging)
+  
+  let firehoseRecord = {
+    DeliveryStreamName: process.env.FIREHOSE_DELIVERY_STREAM_NAME,
+    Record: { 
+        Data: Buffer.from(JSON.stringify(body)+'\n')
+    }
+  }
+  
+  return firehose.putRecord(firehoseRecord).then(() => {
     return successResponse()
   }).catch(err =>{
-    consoleTimeEnd('track', logging)
     return errorResponse(err)
   })
 }
-
-// Send the event with the timestamp and project name to firehose
-function sendToFirehose(projectName, body, receivedAt, log) {
-  body["project_name"] = projectName;
-  body["received_at"] = receivedAt.toISOString();
-  // FIX timestamp must never be in the future
-  if (!body.timestamp) {
-    body["timestamp"] = body["received_at"];
-  }
-  if (!body.message_id) {
-    body["message_id"] = uuidv4()
-  }
-  let firehoseData = Buffer.from(JSON.stringify(body)+'\n')
-  consoleTime('firehose',log)
-  consoleTime('firehose-create',log)
-
-  let firehosePromise = firehose.putRecord({
-    DeliveryStreamName: process.env.FIREHOSE_DELIVERY_STREAM_NAME,
-    Record: { 
-        Data: firehoseData
-    }
-  }).promise().then(result => {
-    consoleTimeEnd('firehose',log)
-    return result
-  })
-  consoleTimeEnd('firehose-create',log)
-  return firehosePromise;
-}
-
 
 function successResponse() {
   return {
@@ -90,16 +60,4 @@ function errorResponse(message) {
 
 function checkShouldLog() {
   return Math.random() < LOG_PROBABILITY;
-}
-
-function consoleTime(name, shouldLog) {
-  if (shouldLog) {
-    console.time(name)
-  }
-}
-
-function consoleTimeEnd(name, shouldLog) {
-  if (shouldLog) {
-    console.timeEnd(name)
-  }
 }
