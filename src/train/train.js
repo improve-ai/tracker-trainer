23c25eb7c8b87964ba995646c48ce9ea7ba1364c
EC2 Default User <local@local>
2021-06-03 01:23:56 +0000

    remove comment

--- src/train/train.js
+++ src/train/train.js
@@ -10,87 +10,20 @@ const s3 = new AWS.S3();
 const sagemaker = new AWS.SageMaker();
 
 const naming = require("./naming.js")
-const customize = require("./customize.js")
 
 module.exports.dispatchTrainingJobs = async () => {
 
   console.log(`dispatching training jobs`)
   return Promise.all(Object.entries(naming.getModelsByProject()).map(([projectName, models]) => 
     Promise.all(models.map(model => 
-      createFeatureTrainingJob(projectName, model)))))
+      createTrainingJob(projectName, model)))))
 }
 
-function createFeatureTrainingJob(projectName, model) {
+function createTrainingJob(projectName, model, trainingJobName) {
   
-  let hyperparameters = { model: model } 
-  if (customize.config.binaryRewards) {
-    Object.assign(hyperparameters,  { binary_rewards: "true" })
-  }
-  
-  const trainingJobName = getFeatureTrainingJobName(projectName, model)
-  
-  var params = {
-    TrainingJobName: trainingJobName,
-    HyperParameters: hyperparameters,
-    AlgorithmSpecification: { /* required */
-      TrainingImage: process.env.FEATURE_TRAINING_IMAGE,
-      TrainingInputMode: "Pipe",
-    },
-    InputDataConfig: [ 
-      {
-        ChannelName: 'train',
-        CompressionType: 'Gzip',
-        DataSource: { 
-          S3DataSource: { 
-            S3DataType:"S3Prefix",
-            S3Uri: naming.getRewardedDecisionTrainS3Uri(projectName, model),
-            S3DataDistributionType: "FullyReplicated",
-          }
-        },
-      },
-      {
-        ChannelName: 'validation',
-        CompressionType: 'Gzip',
-        DataSource: { 
-          S3DataSource: { 
-            S3DataType:"S3Prefix",
-            S3Uri: naming.getRewardedDecisionValidationS3Uri(projectName, model), 
-            S3DataDistributionType: "FullyReplicated",
-          }
-        },
-      },
-    ],
-    OutputDataConfig: { 
-      S3OutputPath: naming.getFeatureModelsS3Uri(projectName, model), 
-    },
-    ResourceConfig: { 
-      InstanceCount: process.env.FEATURE_TRAINING_INSTANCE_COUNT, 
-      InstanceType: process.env.FEATURE_TRAINING_INSTANCE_TYPE,
-      VolumeSizeInGB: 1
-    },
-    RoleArn: process.env.FEATURE_TRAINING_ROLE_ARN,
-    StoppingCondition: { 
-      MaxRuntimeInSeconds: process.env.FEATURE_TRAINING_MAX_RUNTIME_IN_SECONDS,
-    },
-  };
-  
-  console.log(`creating feature training job ${trainingJobName} project ${projectName} model ${model} params ${JSON.stringify(params)}`)
-
-  return sagemaker.createTrainingJob(params).promise().catch(error => {
-    // handle the error locally because the training job should not be re-attempted.
-    // the most likely cause of failure is a configured model that is not yet receiving events
-    console.log(`error creating feature training job ${trainingJobName} project ${projectName} model ${model}`, error)
-  })
-}
-
-
-function createXGBoostTrainingJob(projectName, model, trainingJobName) {
-  
-  // TODO clean up transformed parent shards and out of range dates
   
   console.log(`creating xgboost training job ${trainingJobName} project ${projectName} model ${model} params ${JSON.stringify(params)}`)
   const hyperparameters = naming.getXGBoostHyperparameters(projectName, model)
-  hyperparameters["csv_weights"] = "1"
   
   var params = {
     TrainingJobName: trainingJobName,
@@ -140,26 +73,6 @@ function createXGBoostTrainingJob(projectName, model, trainingJobName) {
   };
 
   return sagemaker.createTrainingJob(params).promise()
-}
-
-module.exports.xgboostModelCreated = async (event, context) => {
-
-  console.log(`processing s3 event ${JSON.stringify(event)}`)
-
-  if (!event.Records || !event.Records.length > 0 || event.Records.some(record => !record.s3 || !record.s3.bucket || !record.s3.bucket.name || !record.s3.object || !record.s3.object.key)) {
-    throw new Error(`WARN: Invalid S3 event ${JSON.stringify(event)}`)
-  }
-  
-  // s3 only ever includes one record per event, but this spec allows multiple, so multiple we will process.
-  return Promise.all(event.Records.map(s3EventRecord => {
-    const s3Key = s3EventRecord.s3.object.key
-
-    // xgboost_models/projectName/modelName/<xgboost training job>/model.tar.gz
-    let [projectName, model, trainingJobName] = s3Key.split('/').slice(1,4)
-
-    // the model is already created when feature training is complete, so just launch the transform
-    return createModelTransformJob(projectName, model, trainingJobName)
-  }))
 }
 
 function getFeatureTrainingJobName(projectName, model) {
