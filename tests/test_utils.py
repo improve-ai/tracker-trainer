73350e50c10c4c90178b65fe9b9850f498545970
Manuel Zanutelli <mclzc89@gmail.com>
2021-10-18 15:10:22 -0500

    add tests for save_gzipped_jsonlines and test_upload_gzipped_jsonlines

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,32 +1,100 @@
 # Built-in imports
-import string
+import gzip
+import json
+import os
+
+# External imports
+import pytest
+import boto3
+from moto import mock_s3
 
 # Local imports
-from utils import _is_valid_model_name
+from utils import save_gzipped_jsonlines
+from utils import upload_gzipped_jsonlines
+from test_history_record import get_record
+
+S3_BUCKET = "temp_bucket"
+S3_KEY    = "temp_key"
 
 
-def test__is_valid_model_name():
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with mock_s3():
+        yield boto3.client('s3', region_name='us-east-1')
+
+
+def test_save_gzipped_jsonlines(tmp_path):
     """
     
-    messages-2.0, songs-2.0, stories-2.0, appconfig
+    Parameters
+    ----------
+    tmp_path :  pathlib.Path
+        A fixture automatically provided by Pytest
     """
 
-    assert _is_valid_model_name(1) == False
-    assert _is_valid_model_name([]) == False
-    assert _is_valid_model_name({}) == False
-    assert _is_valid_model_name(None) == False
-    assert _is_valid_model_name("") == False
+    gzip_file = tmp_path / "temp_test.jsonl.gz"
+    json_dicts = [
+        get_record(),
+        get_record(msg_val="B", type_val="event", model_val="m_name", count_val=2)
+    ]
     
-    punctuation = list(string.punctuation)
-    punctuation.remove(".")
-    punctuation.remove("-")
-    punctuation.remove("_")
-    for i in punctuation:
-        assert _is_valid_model_name(i) == False
-        assert _is_valid_model_name(i+"a") == False
+    save_gzipped_jsonlines(gzip_file, json_dicts)
 
-    for i in string.whitespace:
-        assert _is_valid_model_name(i) == False
-        
-    assert _is_valid_model_name("a") == True
-    assert _is_valid_model_name("aa") == True
+    # Assert only number of files saved to temp dir
+    assert len(list(tmp_path.iterdir())) == 1
+
+    # Assert original contents are equal to saved contents
+    with gzip.open(gzip_file, mode='r') as gzf:
+        for i,line in enumerate(gzf.readlines()):
+            loaded_dict = json.loads(line)
+            original_dict = json_dicts[i]
+            assert len(original_dict.keys()) == len(loaded_dict.keys())
+            assert all([a == b for a, b in zip(original_dict.keys(), loaded_dict.keys())])
+
+
+
+def test_test_upload_gzipped_jsonlines(s3, mocker):
+    """
+
+    Parameters
+    ----------
+    s3 : boto3.client
+        Custom fixture which provides a mocked S3 client
+    mocker : pytest_mock.plugin.MockerFixture
+        Fixture provided by pytest-mocker
+    """
+
+    # Create a bucket in the Moto's virtual AWS account
+    s3.create_bucket(Bucket=S3_BUCKET)
+
+    # Replace the S3 client used in the app with a mock
+    mocker.patch('config.s3client', new=s3)
+
+    json_dicts = [
+        get_record(),
+        get_record(msg_val="B", type_val="event", model_val="m_name", count_val=2) ]
+
+    upload_gzipped_jsonlines(S3_BUCKET, S3_KEY, json_dicts)
+
+    response = s3.get_object(
+        Bucket=S3_BUCKET,
+        Key=S3_KEY
+    )
+
+    with gzip.GzipFile(fileobj=response['Body']) as gzf:
+        for i,line in enumerate(gzf.readlines()):
+            loaded_dict = json.loads(line)
+            original_dict = json_dicts[i]
+            assert len(original_dict.keys()) == len(loaded_dict.keys())
+            assert all([a == b for a, b in zip(original_dict.keys(), loaded_dict.keys())])
+
+
