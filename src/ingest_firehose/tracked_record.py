47eef921509f9e8bc2a0a0a581649ac308109d3b
EC2 Default User <ec2-user@ip-10-0-0-172.ec2.internal>
2021-11-01 20:24:29 +0000

    implemented to_rewarded_decision_dict

--- src/ingest_firehose/tracked_record.py
+++ src/ingest_firehose/tracked_record.py
@@ -1,5 +1,5 @@
 # Built-in imports
-import json
+import orjson as json
 import re
 import dateutil
 import gzip
@@ -15,27 +15,24 @@ import src.train.constants as tc
 
 
 MESSAGE_ID_KEY = 'message_id'
+DECISION_ID_KEY = 'decision_id'
 TIMESTAMP_KEY = 'timestamp'
 TYPE_KEY = 'type'
 DECISION_TYPE = 'decision'
 REWARD_TYPE = 'reward'
-EVENT_TYPE = 'event'
-EVENT_KEY = EVENT_TYPE
 MODEL_KEY = 'model'
-REWARD_KEY = 'reward'
+REWARD_KEY = REWARD_TYPE
+REWARDS_KEY = 'rewards'
 VARIANT_KEY = 'variant'
 GIVENS_KEY = 'givens'
 COUNT_KEY = 'count'
 SAMPLE_KEY = 'sample'
 RUNNERS_UP_KEY = 'runners_up'
-PROPERTIES_KEY = 'properties'
-VALUE_KEY = 'value'
+
 
 class TrackedRecord:
     # slots are faster and use much less memory than dicts
-    __slots__ = [MESSAGE_ID_KEY, TIMESTAMP_KEY, TYPE_KEY, MODEL_KEY, EVENT_KEY, PROPERTIES_KEY,
-                 VALUE_KEY, REWARD_KEY, VARIANT_KEY, GIVENS_KEY, COUNT_KEY, RUNNERS_UP_KEY, SAMPLE_KEY]
-
+    __slots__ = [MESSAGE_ID_KEY, TIMESTAMP_KEY, TYPE_KEY, MODEL_KEY, DECISION_ID_KEY, REWARD_KEY, VARIANT_KEY, GIVENS_KEY, COUNT_KEY, RUNNERS_UP_KEY, SAMPLE_KEY]
     
     def __init__(self, json_dict: dict):
         assert isinstance(json_dict, dict)
@@ -55,16 +52,16 @@ class TrackedRecord:
         self.model = json_dict.get(MODEL_KEY)
         if not _is_valid_model_name(self.model):
             self.model = None
-            
-        self.event = json_dict.get(EVENT_KEY)
 
-        self.properties = json_dict.get(PROPERTIES_KEY)
-        
-        self.value = 0.0
-        if self.is_event_record():
-            self.value = config.DEFAULT_EVENT_VALUE
-            if isinstance(self.properties, dict):
-                self.value = self.properties.get(VALUE_KEY, self.value)
+        self.decision_id = json_dict.get(DECISION_ID_KEY)
+            
+        # parse and validate reward
+        reward = json_dict.get(REWARD_KEY, 0)
+
+        if not isinstance(reward, (int, float)):
+            raise ValueError('invalid reward')
+
+        self.reward = reward
 
         # all variant values are valid
         self.variant = json_dict.get(VARIANT_KEY)
@@ -104,93 +101,41 @@ class TrackedRecord:
         return self.type == DECISION_TYPE
         
         
-    def is_event_record(self):
-        return self.type == EVENT_TYPE
-        
-        
     def is_reward_record(self):
         return self.type == REWARD_TYPE
 
-        
-    def reward_window_contains(self, other):
-        return other.timestamp >= self.timestamp and other.timestamp <= self.timestamp + config.REWARD_WINDOW
-            
         
     def to_rewarded_decision_dict(self):
         """ Return a dict representation of the decision record """
         
         result = {}
         
-        result[TIMESTAMP_KEY] = self.timestamp.isoformat()
-        result[REWARD_KEY] = self.reward
-        result[VARIANT_KEY] = self.variant
-        
-        if self.givens is not None:
-            result[GIVENS_KEY] = self.givens
-        if self.count is not None:
-            result[COUNT_KEY] = self.count
-        if self.runners_up is not None:
-            result[RUNNERS_UP_KEY] = self.runners_up
-        if self.sample is not None:
-            result[SAMPLE_KEY] = self.sample
+        if self.is_decision_record():
+            result[DECISION_ID_KEY] = self.message_id
+            result[TIMESTAMP_KEY] = self.timestamp.isoformat()
+            result[VARIANT_KEY] = json.dumps(self.variant)
+            
+            if self.givens is not None:
+                result[GIVENS_KEY] = json.dumps(self.givens)
+                
+            if self.count is not None:
+                result[COUNT_KEY] = self.count
+                
+            if self.runners_up is not None:
+                result_runners_up = []
+                for runner_up in self.runners_up:
+                    result_runners_up.append(json.dumps(runner_up))
+                result[RUNNERS_UP_KEY] = result_runners_up
+                
+            if self.sample is not None:
+                result[SAMPLE_KEY] = json.dumps(self.sample)
+                
+        elif self.is_reward_record():
+            result[DECISION_ID_KEY] = self.decision_id
+            result[REWARDS_KEY] = { self.message_id: self.reward }
             
         return result
 
-
-def _load_records(file: pathlib.Path, message_ids: set) -> list:
-    """
-    Load records from a gzipped jsonlines file
-
-    Parameters
-    ----------
-    file : path-like
-        Path of the input gzipped jsonlines file to load
-    message_ids : an empty set ?
-        previously loaded message_ids to filter out in the event of 
-        duplicates
-
-    Returns
-    -------
-    list
-        A list of records
-    """
-
-    line_count = 0
-
-    records = []
-    error = None
-
-    try:
-        with gzip.open(file.absolute(), mode="rt", encoding="utf8") as gzf:
-            for line in gzf.readlines():
-                line_count += 1  # count every line as a record whether it's parseable or not
-                # Do a inner try/except to try to recover as many records as possible
-                try:
-                    record = HistoryRecord(json.loads(line))
-                    
-                    assert isinstance(record.message_id, str)
-                    if record.message_id not in message_ids:
-                        message_ids.add(record.message_id)
-                        records.append(record)
-                    else:
-                        config.stats.incrementDuplicateMessageIdCount()
-                except (json.decoder.JSONDecodeError, ValueError, AssertionError) as e:
-                    error = e
-    except (zlib.error, EOFError, gzip.BadGzipFile) as e:
-        # gzip can throw zlib.error, EOFError, or gzip.BadGZipFile on corrupt file
-        error = e
-
-    if error:
-        # Unrecoverable parse error, copy the file to /unrecoverable
-        dest = config.UNRECOVERABLE_PATH / file.name
-        print(
-            f'unrecoverable parse error "{error}", copying {file.absolute()} to {dest.absolute()}')
-        utils.copy_file(file, dest)
-        config.stats.incrementUnrecoverableFileCount()
-
-    config.stats.incrementHistoryRecordCount(line_count)
-
-    return records
 
 
 def _is_valid_model_name(model_name):   
