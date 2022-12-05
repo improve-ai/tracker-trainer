441f6f0785e05d2b4670a7eb06896127a2dc7478
Konrad Wiecko <konrad.wiecko@gmail.com>
2022-12-05 20:33:40 +0100

    increased number of groom iterations

--- src/ingest/config.py
+++ src/ingest/config.py
@@ -8,7 +8,7 @@ S3_CONNECTION_COUNT = 16
 # boto3 client must be pre-initialized for multi-threaded (https://github.com/boto/botocore/issues/1246)
 s3client = boto3.client("s3", config=botocore.config.Config(max_pool_connections=S3_CONNECTION_COUNT))
 
-MAX_GROOM_ITERATIONS = 10
+MAX_GROOM_ITERATIONS = 30 # one iteration takes about one minute
 
 PARQUET_FILE_MAX_DECISION_RECORDS = 10000
 
