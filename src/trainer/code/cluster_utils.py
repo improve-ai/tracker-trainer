cb2bbbe2f76e0b6b0a3651b52d837f7b96370b6d
KWiecko <konrad.wiecko@gmail.com>
2023-04-21 00:28:30 +0200

    Implemented spawning multiple workers per node (if possible; assuming that for each 4 vcores 1 worker should be spawned).

--- src/trainer/code/cluster_utils.py
+++ src/trainer/code/cluster_utils.py
@@ -3,6 +3,8 @@ import socket
 from subprocess import Popen
 import time
 
+from config import CORES_PER_WORKER
+
 
 def get_ip_from_host(host_name) -> str:
     """
@@ -64,6 +66,8 @@ def start_daemons(
     cmd_start_worker = \
         'dask-worker' if not dask_pth \
         else os.path.join(dask_pth, 'dask-worker')
+    # following https://github.com/dask/distributed/issues/7523 it might be
+    # beneficial to spawn 1 worker for 4 cores available
 
     # dask-worker scheduler:8786 --nprocs 3 --nthreads 2 --resources "process=1"
     # res_cmd = ['--nprocs', '1', '--nthreads', '2', '--memory-limit=4e9']
@@ -72,8 +76,16 @@ def start_daemons(
     # WARN: BE CAREFUL ABOUT INCREASING -nprocs FROM 1.  RESOURCES ARE PER
     # PROCESS AND WE USE RESOURCES TO SINGLE THREAD ACCESS TO THE INPUT PIPE
     #
+
     # Each worker has 1 records pipe resource to ensure single threaded access
     res_cmd = ['--death-timeout', '10']
+
+    # TODO using multiple workers per node seems to speed things up
+    #  (maybe this would also solve the work-stealing bug?)
+    # n_worker_processes = 1
+    n_worker_processes = os.cpu_count() // CORES_PER_WORKER
+    if n_worker_processes > 1:
+        res_cmd += ['--nworkers', str(n_worker_processes)]
 
     schedule_conn_string = f'tcp://{master_ip}:8786'
 
@@ -87,9 +99,10 @@ def start_daemons(
 
         time.sleep(1)
         print('starting worker')
+        print(f'Start worker CMD: {res_cmd}')
         worker_process = \
             Popen([cmd_start_worker, schedule_conn_string] + res_cmd)
     except Exception as exc:
         print(exc)
 
-    return scheduler_process, worker_process
+    return scheduler_process, worker_process, n_worker_processes
